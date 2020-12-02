242c3f2bbbefaa04294ca81eb2f2efa33e482de0
renzo_arenaza <renzo.arenaza@sura.pe>
2020-12-02 15:19:58 -0500

    prueba

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -11,9 +11,19 @@ public class ResponderModel {
             return teamName;
         }
 
+        Integer number1;
+        Integer number2;
+
         Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
         if (sumMatcher.matches()) {
             return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
+        }
+
+        Matcher compareMatcher = Pattern.compile(".*which of the following numbers is the largest: (\\d+), (\\d+)").matcher(question);
+        if (compareMatcher.matches()) {
+            number1 = Integer.parseInt(compareMatcher.group(1));
+            number2 = Integer.parseInt(compareMatcher.group(2));
+            return String.valueOf( number1>number2 ? number1 : number2);
         }
         
         return teamName;
---
+++ src/test/java/com/serverless/ResponderModelTest.java
@@ -0,0 +1,47 @@
+package com.serverless;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+public class ResponderModelTest {
+
+    ResponderModel responder= new ResponderModel();
+
+    @Test
+    public void cuandoLaPreguntaSea () {
+        //arrange
+        String pregunta = "";
+
+        //act
+        String respuesta = responder.answer(pregunta);
+
+        //assert
+        assertThat(respuesta).isEqualTo("peru");
+
+    }
+
+    @Test
+    public void cuandoLaPreguntaSuma15y2Responde17 () {
+        //arrange
+        String pregunta = "what is the sum of 15 and 2";
+
+        //act
+        String respuesta = responder.answer(pregunta);
+
+        //assert
+        assertThat(respuesta).isEqualTo("17");
+
+    }
+
+    @Test
+    public void cuandoLaPreguntaCompara80y870Responde870 () {
+        //arrange
+        String pregunta = "which of the following numbers is the largest: 80, 870";
+
+        //act
+        String respuesta = responder.answer(pregunta);
+
+        //assert
+        assertThat(respuesta).isEqualTo("870");
+
+    }
+}
